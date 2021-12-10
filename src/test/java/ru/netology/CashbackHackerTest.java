963452643c2cea022de1458c39abad1388d88764
UBCh <u2b127@gmail.ru>
2021-12-10 13:19:39 +0300

    initialization of the project - branch junit4

--- src/test/java/ru/netology/CashbackHackerTest.java
+++ src/test/java/ru/netology/CashbackHackerTest.java
@@ -1,9 +1,9 @@
 package ru.netology;
 
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackerTest {
     CashbackHacker cashbackHacker = new CashbackHacker();
@@ -12,27 +12,27 @@ public class CashbackHackerTest {
     public void shouldRemain1000() {
         int expected = 0;
         int actual = cashbackHacker.remain(1000);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void shouldRemain2000() {
         int expected = 10;
         int actual = cashbackHacker.remain(2000);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void shouldRemain900() {
         int expected = 0;
         int actual = cashbackHacker.remain(900);
-        assertEquals(actual, expected, "to receive bonuses, increase the purchase amount to 1100 rubles.");
+        assertEquals(expected, actual);
     }
 
     @Test
     public void shouldRemain1100() {
         int expected = 1;
         int actual = cashbackHacker.remain(1100);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
