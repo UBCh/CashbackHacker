ae5aa6543f171fed2c7e827fd1d2019694b17dca
UBCh <u2b127@gmail.ru>
2021-12-10 14:13:56 +0300

    the tests pass

--- src/test/java/ru/netology/CashbackHackerTest.java
+++ src/test/java/ru/netology/CashbackHackerTest.java
@@ -10,29 +10,29 @@ public class CashbackHackerTest {
 
     @Test
     public void shouldRemain1000() {
-        int expected = 0;
+        int expected = 1000;
         int actual = cashbackHacker.remain(1000);
         assertEquals(actual, expected);
     }
 
     @Test
     public void shouldRemain2000() {
-        int expected = 10;
-        int actual = cashbackHacker.remain(2000);
+        int expected = 1;
+        int actual = cashbackHacker.remain(1999);
         assertEquals(actual, expected);
     }
 
     @Test
     public void shouldRemain900() {
-        int expected = 0;
+        int expected = 100;
         int actual = cashbackHacker.remain(900);
-        assertEquals(actual, expected, "to receive bonuses, increase the purchase amount to 1100 rubles.");
+        assertEquals(actual, expected, "to receive bonuses, increase the purchase amount to 1000 rubles.");
     }
 
     @Test
     public void shouldRemain1100() {
-        int expected = 1;
-        int actual = cashbackHacker.remain(1100);
+        int expected = 950;
+        int actual = cashbackHacker.remain(1050);
         assertEquals(actual, expected);
     }
 }
